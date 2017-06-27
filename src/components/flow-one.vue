478cd7ce2d45fec7d7228c71f717dc236bdd2cae
Chaoyi Feng <fengchaoyi@meituan.com>
2017-06-27 16:54:06 +0800

    Add some babel dependencies, use flow in 'flow-one.vue' file, but it seems not working.

---
+++ src/components/flow-one.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>I'm made with Flow!</p>
+        <p>{{wrongFunction(isAString)}}</p>
+    </div>
+</template>
+
+<script>
+    /* @flow */
+
+    const isAString: string = 'I am a string!';
+
+    export default {
+        data() {
+            return {
+                isAString
+            }
+        },
+        methods: {
+            wrongFunction(iAmAString: string) {
+                return 2 * iAmAString + isAString;
+            }
+        }
+    }
+</script>
